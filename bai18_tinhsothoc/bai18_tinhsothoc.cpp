b9e73881f631457590de8ea1ac7a8496276b905c
tutai000 <tutainguyen34@gmail.com>
2024-11-25 09:02:23 +0700

    Add project files.

---
+++ bai18_tinhsothoc/bai18_tinhsothoc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    cout << "TINH SO THOC TREN BAN CO VUA\n";
+    int SoO;
+    double tongThoc = 0;
+    cout << "nhap so o: "; cin >> SoO;
+
+    for (int i = 0; i < SoO; i++)
+    {
+        tongThoc = tongThoc + pow(2, i);
+    }
+    cout << "TONG SO THOC LA" << tongThoc;
+
+    return 0;
+
+}
